ff40e4bdc3e585f1e609e64761d70333fc3ec381
thebugjim <aaronn321@gmail.com>
2014-12-11 15:31:53 -0800

    test

--- js/jquery.js
+++ js/jquery.js
@@ -8,7 +8,7 @@ $(function() {
   	console.log ($(this).attr('href'));
   	console.log (window.location.pathname);
   	console.log (window.location.pathname.indexOf('.html') > -1);
-    if (window.location.pathname.indexOf($(this).attr('href')  > -1 || window.location.pathname.indexOf('.html') == -1) {
+    if (window.location.pathname.indexOf($(this).attr('href'))  > -1 || window.location.pathname.indexOf('.html') == -1) {
       $(this).addClass('currentnav');
     }
   });
