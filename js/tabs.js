f3a70229a7df820d913bff476ecf6888cbf492c8
Andrei Anashkin <andreigiant@gmail.com>
2014-12-12 17:55:57 -0800

    templated char page

---
+++ js/tabs.js
@@ -0,0 +1,14 @@
+var tabContent = {
+  category: 'root',
+  subcategories: [
+    {category: 'Ajax (programming)'},
+    {category: 'JavaScript engines'},
+    {category: 'JavaScript programming languages family',
+     subcategories: [{
+       category: 'JavaScript dialect engines'
+     }]
+    },
+    {category: 'JavaScript based calendar components'},
+    {category: 'JavaScript based HTML editors'}
+  ]
+};
